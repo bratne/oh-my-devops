95198451d31efcf1ed1c560a5db5bccdc6e868a8
Øyvind Bratne <89254037+bratne@users.noreply.github.com>
2025-05-26 23:38:44 +0200

    New version with working installscript and executable oobe-script

--- arch/install.cmd
+++ arch/install.cmd
@@ -1,9 +1,10 @@
 wsl --install -d Debian
-set "wslcommand=wsl -d Debian"
+set "wslcommand=wsl -d Debian --cd ~"
 %wslcommand% apt-get update
 %wslcommand% apt-get install git make docker.io -y
 %wslcommand% git clone https://github.com/bratne/oh-my-devops
 %wslcommand% make -C oh-my-devops/arch
 %wslcommand% make -C oh-my-devops/arch wslexport	
-#wsl --unregister Debian
+:: wsl --unregister Debian
+%wslcommand% mv oh-my-devops/arch/arch.tar `wslpath -u "%CD%"`
 wsl --install --from-file arch.tar
